1cfbd38d01ea6bea5d5ad5760de535813948baf4
Miguel Muñoz Caro <miguel.munoz2016@twk.cl>
2017-06-30 09:17:49 -0400

    Botón pause

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,18 @@ function verVideo(vid){
   var vidFrame = document.getElementById('vid_frame');
   vidFrame.setAttribute("src", v);
 
-  //Volver fondo blanco a la lista
+  //Volver fondo blanco a la lista, pause.
   var x = document.getElementsByClassName("video-link");
   var i;
   for (i = 0; i < x.length; i++) {
     x[i].style.backgroundColor = "#fff";
+    x[i].getElementsByTagName("I")[0].innerHTML = "play_circle_filled";
   }
 
   //Fondo verde a link seleccionado
   var link = document.getElementById('video-link-id'+(vid+1));
   link.setAttribute("style", 'background-color: #d1fcad;');
+
+  link.getElementsByTagName("I")[0].innerHTML = "pause_circle_outline";
 
 }
